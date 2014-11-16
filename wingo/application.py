c68b800d60c70a01eb0adf7051c64fa5d71dbafe
schutz <sacha@labsquare.org>
2014-11-16 02:04:51 +0100

    session doesn't work.. user not finished.. too tired

---
+++ wingo/application.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask
+from flask.ext import restful
+from flask import render_template
+import mongoengine as mongo
+from models import *
+
+import os
+
+# Transform all exception as a Json Error
+# class ExceptionAwareApi(restful.Api):
+#     def handle_error(self, e):
+
+#     	message = str(e)
+#     	if hasattr(e,"data"):
+# 			data = getattr(e, 'data')
+# 			if "message" in data:
+# 				message = data["message"]
+
+# 	code= 400
+# 	results = ErrorResponse(message,code)
+# 	return self.make_response(results, code)
+
+''' initialization of application '''
+app = Flask(__name__)
+api = restful.Api(app)
+
+'''load configuration from config.py '''
+app.config.from_pyfile("config.py")
+mongo.connect(app.config["DATABASE"])
+
+''' Create upload directory if not present '''
+if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+	os.makedirs(app.config["UPLOAD_FOLDER"])
+
+
+
+
+
+@app.before_request
+def check_request():
+	return
+
+# We want to access app from resources..
+from resources.notes import *
+from resources.comments import *
+from resources.config import *
+from resources.tags import *
+from resources.users import *
+
+api.add_resource(NoteCollection, '/notes')
+api.add_resource(NoteResource, '/notes/<string:note_id>')
+api.add_resource(NoteUploadResource, '/notes/picture')
+api.add_resource(NoteDownloadResource, '/notes/picture/<string:filename>')
+
+api.add_resource(UserLogin, '/users/login')
+api.add_resource(UserLogout, '/users/logout')
+api.add_resource(UserMe, '/users/me')
+
+
+
+api.add_resource(CommentCollection, '/notes/<string:note_id>/comments')
+#api.add_resource(CommentResource, '/comment/<string:comment_id>')
+api.add_resource(ConfigResource, '/config')
+api.add_resource(TagResource, '/tags')
+
+
+
+	 
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
--- wingo/resources/users.py
+++ wingo/resources/users.py
@@ -1,20 +1,53 @@
 from flask import Flask
 from flask import request
+from flask import session
 from flask.ext import restful
 from flask.ext.restful import reqparse
 from flask import current_app
-from . util import SuccessResponse,ErrorResponse
-from models import Note
-from app import app
+# from flask_login import login_user, current_user 
 
+from . util import SuccessResponse,ErrorResponse
+from models import User
 # 'wingo' import must be done from root level (app, test, dbGen, ...)
 #from models import Note
 #from common.util import *
 
 
+class UserLogin(restful.Resource):
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('email',    type=str,   help='email is not defined', required = True)
+		parser.add_argument('password',    type=str,   help='Password is not defined', required = True)
+		
+		args = parser.parse_args()
+
+		email = args["email"]
+		password = args["password"]
+		user = User.objects(email=email, password=password).first()
+	
+		if user is None:
+			return ErrorResponse("Bas email or password")
+
+		else:
+			session['user'] = "sadsfsfsf"
+
+		return SuccessResponse()
+		
 
 
 
+class UserLogout(restful.Resource):
+	def post(self):
+		return "logout"
+
+
+class UserMe(restful.Resource):
+	def get(self):
+		print(session)
+		if "current_user_id" not in session:
+			return ErrorResponse("not logged")
+		else:
+			return SuccessResponse(session["current_user_id"])
 
 
 class UserNote(restful.Resource):
