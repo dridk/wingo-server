79a653645641fd8919bcf254aa86b1f0ff7c17f7
schutz <sacha@labsquare.org>
2014-12-06 17:13:28 +0100

    set alpha 0.2 version Thorfin

--- dbtools/generate.py
+++
@@ -1,7 +0,0 @@
-from loremipsum import *
-import math
-from datetime import *
-from random import randint
-from . utils import *
-
-
--- wingo/config.py
+++ wingo/config.py
@@ -1,8 +1,8 @@
 DEBUG                     = True
 SECRET_KEY                ="writeyourownsecretkey"
 DATABASE                  ="wingo"
-VERSION                   ="1.0"
-VERSION_NAME              ="Xu Fu"
+VERSION                   ="0.2"
+VERSION_NAME              ="Thorfinn"
 ALLOWED_RADIUS            = [100,500,1000,5000000000]
 MAX_NOTE_LENGTH           = 255
 COUNT_PER_PAGE            = 20
