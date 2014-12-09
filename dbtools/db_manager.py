3f19de5ec0c3944506651fa9bc84527e66c57154
Sacha schutz <sacha@labsquare.org>
2014-12-09 20:50:31 +0100

    add Image generation in dbtools

--- dbtools/db_manager.py
+++ dbtools/db_manager.py
@@ -5,6 +5,8 @@ import random
 from . utils import *
 from progress.bar import *
 from datetime import *
+import os
+import shutil
 
 manager = Manager(usage="Perform database operations")
 
@@ -17,8 +19,8 @@ def drop():
 
 
 
-@manager.option('-lat', '--latitude', help='latitude of the center', default=48.386537, type=float)
-@manager.option('-lon', '--longitude', help='longitude of the center', default=-4.490095, type=float)
+@manager.option('-lat', '--latitude', help='latitude of the center', default=43.8174759, type=float)
+@manager.option('-lon', '--longitude', help='longitude of the center', default=-79.4210148, type=float)
 @manager.option('-r', '--radius', help='radius in meter', default=5000, type=int)
 @manager.option('-c', '--count', help='number of note', default=100,type=int)
 @manager.option('-u', '--userCount', help='number of user', default=10,type=int)
@@ -26,12 +28,25 @@ def generate(latitude, longitude, radius, count, userCount):
 	"generate random notes at specific place"
 
 
-	users = list()
+	users  = list()
+	photos = ["http://www.toulon-hyeres.aeroport.fr/var/storage/images/media/images/bes12/13851-1-fre-FR/bes1.jpg",
+			  "http://knightsinntoronto-com.factorepreview.ca/system/images/images/2/original/toronto-012222.jpg?1358279327"]
+	
+	# Add default dev user 
+	devUser = User(email="sacha@labsquare.org",
+					  password="sacha",
+					  nickname="dridk", 
+					  avatar="http://www.gravatar.com/avatar/2381d1dbfc1450b861bd808424d01943?s=48&d=identicon")
+	devUser.save()
+
+
 	bar = Bar('Generating users...', max=userCount)
 	for i in range(userCount):
 		users.append(genUser())
 		bar.next()
 	bar.finish()
+
+
 
 
 	bar = Bar('Generating note...', max=count)
@@ -42,6 +57,8 @@ def generate(latitude, longitude, radius, count, userCount):
 		note.author = random.choice(users)
 		note.message = "{} {}".format(genText(), genTags(random.randint(1,3)))
 		note.location= list(pos)
+		if random.randint(1, 5) == 1:
+			note.picture = random.choice(photos)
 		note.save()
 		bar.next()
 	bar.finish()
